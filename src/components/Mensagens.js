78e771a98f72a10cf8016a14909c0a0aed291ee0
Gustavo Leão <gus.leaono@gmail.com>
2021-04-16 17:58:14 +0000

    mensagens

--- src/components/Mensagens.js
+++ src/components/Mensagens.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Mensagens(props){
   return  <div className="padding-5 blue-telegram-2">
-            {contatos.map((indice, item) =>{
-                  return <div>{item}</div>;
+            {props.contatos.map((value, index) =>{
+                  console.log(index,value)
+                  return  <div className="container-logotipo">
+                            <img src={value['imagem']} className="item icon-large border-circle" />
+                            <div className="container-logotipo-coluna">
+                              <h3 className="item">{value['nome']}</h3>
+                              <p className="item">{value['ultima_mensagem']}</p>
+                            </div>
+                          </div>
               })}
           </div>
 }
