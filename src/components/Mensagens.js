975544c34cf55972127b19b7123bfda6fe1c7f51
Gustavo Leão <gus.leaono@gmail.com>
2021-04-16 16:48:25 +0000

    bugs arrumados

--- src/components/Mensagens.js
+++ src/components/Mensagens.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Mensagens(props){
   return  <div className="padding-5 blue-telegram-2">
             {contatos.map((indice, item) =>{
-                  return item
+                  return <div>{item}</div>;
               })}
           </div>
 }
